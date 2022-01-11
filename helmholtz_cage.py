7c85de93622da2e106cc54a3fdf625ed8935171a
B. S. Deng <61472308+bsdeng@users.noreply.github.com>
2022-01-12 00:07:27 +0800

    Add files via upload

--- helmholtz_cage.py
+++ helmholtz_cage.py
@@ -1,7 +1,13 @@
 """
+
+. /Users/bsdeng/opt/anaconda3/bin/activate && conda activate /Users/bsdeng/opt/anaconda3/envs/star_tracker;
+cd /Users/bsdeng/Desktop/code/helmholtz_cage
+pylint helmholtz_cage.py
+python helmholtz_cage.py
+
 Written by B. S. Deng January 6th, 2022.
 
-It solves the problem of Helmholtz Cage design.
+It solves the problem of Square-Shaped Helmholtz Cage design.
 The optimization problem is subject to the cage size, to the current, and the number of turns.
 
 maximize    Vector_Field
@@ -12,14 +18,6 @@ s.t.        current
 where [i], [n], and [a] are the optimization variables.
 This problem can be easily solved via geometric programming.
 We use the equations presented on the following references:
-
-[1] E. Cayo, J. Pareja, P. E. R. Arapa, "Design and implementation of a geomagnetic field simulator for small satellites
-," Conference: III IAA Latin American Cubesat Workshop, Jan. 2019.
-[2] R. C. D. Silva, F. C. Guimaraes, J. V. L. D. Loiola, R. A. Borges, S. Battistini, and C. Cappelletti, "Tabletop
-testbed for attitude determination and control of nanosatellites," Journal of Aerospace Engineering, vol. 32, no. 1,
-2018.
-[3] J. Stevens, "CubeSAT ADCS Validation and Testing Apparatus," Western Michigan University, 2016
-[4] N. Theoret, "Attitude Determination Control Testing System," Western Michigan University, 2016
 """
 
 # Import the required libraries
@@ -28,159 +26,183 @@ import cvxpy as cp
 import numpy as np
 
 
-class CageOpt:
-
+class Cage:
+    """
+    This class contains the parameter values that will be used in the optimization problem.
+    ---------
+    Atributes
+    ---------
+    u_0   : float .:. permeability of free space [-]
+    gamma : float .:. constant value for uniform vector field [-]
+    ub_i  : float .:. current upper bound [A]
+    turns : int   .:. number of coil turns [-]
+    ub_a  : float .:. half length upper bound [m]
+    lb_a  : float .:. half length lower bound [m]
+    des_b : float .:. desired magnetic field intensity at the center of the cage [Tesla]
+    """
     def __init__(self):
-        self.u0 = 4*np.pi*10**-7  # permeability of free space [-]
-        self.pi = np.pi           # pi cte = 3.14 [-]
+        """
+        Constructs all the necessary attributes for the cage object.
+        """
+        self.u_0 = 4*np.pi*10**-7  # permeability of free space [-]
         self.gamma = 0.5445       # for a uniform vector field, this value should be 0.5445 [-]
-        self.max_i = 20           # the maximum allowed current [A]
-        self.turns = 20           # the number of turn [-]
-        self.max_a = 1            # the maximum allowed size [m]
-        self.min_a = 1            # the minimum allowed size [m]
-        self.des_b = 0.00015      # the desired magnetic field value [Tesla]
+        self.current_limits = [0, 20]    # allowed current range [A]
+        self.turns_limits = [0, 20]      # allowed turns range [-]
+        self.length_limits = [1, 1]      # allowed length range [m]
+        self.b_limits = [0.00015, 0.00015]     # the desired magnetic field value [Tesla]
+    def info_objective(self):
+        """
+        It prints the info about the class.
+        """
+        print('This class stores the optimization parameters.')
+        print('The intensity at the center of the cage is ranging between ', \
+              self.b_limits[0], ' to ', self.b_limits[1], 'Tesla.')
+    def info_constraints(self):
+        """
+        It prints the info about the class.
+        """
+        print('This class stores the optimization parameters.')
+        print('The current is ranging between ', \
+              self.current_limits[0], ' to ', self.current_limits[1], 'Tesla.')
+        print('The number of turns is ranging between ', \
+              self.turns_limits[0], ' to ', self.turns_limits[1], 'Tesla.')
+        print('The half-length is ranging between ', \
+              self.length_limits[0], ' to ', self.length_limits[1], 'Tesla.') 
+        
+class OptimalCage:
+    def __init__(self, length, distance, current, turns):
+        # optimal values
+        self.opt_length = length
+        self.opt_distance = distance
+        self.opt_current = current
+        self.opt_turns = turns
 
-    def opt_formulation(self):
-        """ Define the list of variables and parameters """
-        # Variables and parameters must be positive, ie, they must be constructed with the option `pos=True`
-        # Decision Variables
-        n = cp.Variable(pos=True)  # number of turns
-        i = cp.Variable(pos=True)  # current flowing through the coil
-        a = cp.Variable(pos=True)  # half-length of the cage
-        # Parameter values (both must be positive)
-        u0 = cp.Constant(self.u0)
-        pi = cp.Constant(self.pi)
-        gamma = cp.Constant(self.gamma)
-        """ Declare the objective function """
-        # the objective function will be the intensity of the magnetic field at the center of the cage
-        objective = n*i*(4*u0)/(a*pi*(1+gamma**2)*cp.sqrt(2+gamma**2))
-        obj = cp.Minimize(objective)
-        """ Declare the constraints """
-        # create a blank constraint array
-        con = []
-        # the minimal intensity at the center of the cage should be greater than 150 uT
-        con += [n*i*(4*u0)/(a*pi*(1+gamma**2)*cp.sqrt(2+gamma**2)) >= self.des_b]
-        # the maximum current [i] flowing through the coils should be smaller than 20 A
-        # the maximum current value depends on your available power supply
-        # you also should check the wire current limits.
-        con += [i <= self.max_i]
-        # the minimum number of turns [n] should be greater than 30
-        # the value n will impact on your total mass and budget.
-        con += [n == self.turns]
-        # the half-length of the cage [a] should be not be smaller than 1 m
-        # the minimum length of the cage should be at least double of your satellite size
-        # the maximum size will depend on your available free space.
-        con += [a <= self.max_a]
-        con += [a >= self.min_a]
-        """ Formulate and solve the optimization problem """
-        pro = cp.Problem(obj, con)
-        pro.solve(gp=True)
-        """ Print the obtained results """
-        print("Optimal value: ", pro.value, " Tesla")
-        print("Cage length: ", 2*a.value, " meters")
-        print("Cage current: ", i.value, " A")
-        print("Coil number of turns", n.value)
-        return a.value, i.value, n.value
+def squared_cage_optimization():
+    '''
+    Formulate and Solve the optimization problem.
+    Args:
+        max_i = maximum allowed current
+        turns = maximum allowed number of turns
+        max_a = maximum allowed half-length
+        min_a = minimum allowed half-length
+        des_b = the desired magnetic field intensity at the center of the cage.
+    Returns:
+        opt_a = the optimal half-length.
+        opt_n = the optimal number of turns.
+        opt_i = the optimal current.
+    '''
+    # if ,,, << ,,,
+    # raise ValueEror()
+    # optimization variables
+    opt_n = cp.Variable(pos=True)  # number of turns
+    opt_i = cp.Variable(pos=True)  # current flowing through the coil
+    opt_a = cp.Variable(pos=True)  # half-length of the cage
+    # constants
+    u_0 = cp.Constant(Cage().u_0)
+    gamma = cp.Constant(Cage().gamma)
+    # objective function
+    objective = opt_n*opt_i*(4*u_0)/(opt_a*np.pi*(1+gamma**2)*cp.sqrt(2+gamma**2))
+    obj = cp.Minimize(objective)
+    # constraints
+    con = []
+    con += [opt_n*opt_i*4*u_0/(opt_a*np.pi*(1+gamma**2)*cp.sqrt(2+gamma**2)) <= Cage().b_limits[1]]
+    con += [opt_n*opt_i*4*u_0/(opt_a*np.pi*(1+gamma**2)*cp.sqrt(2+gamma**2)) >= Cage().b_limits[0]]
+    con += [opt_i <= Cage().current_limits[1]]
+    con += [opt_n == Cage().turns_limits[1]]
+    con += [opt_a <= Cage().length_limits[1]]
+    con += [opt_a >= Cage().length_limits[0]]
+    # Construct & Solve the optimization problem.
+    pro = cp.Problem(obj, con)
+    pro.solve(gp=True)
+    print("Optimal value: ", pro.value, " Tesla")
+    print("Cage Half-Length: ", opt_a.value, " meters")
+    print("Cage current: ", opt_i.value, " A")
+    print("Coil number of turns", opt_n.value)
+    length = 2*opt_a.value
+    distance = 2*(Cage().gamma)*opt_a.value
+    opt_design = OptimalCage(length, distance, opt_i.value, opt_n.value)
+    return opt_design
 
+def vertical_wire(coordinate, y_pos, z_pos):
+    '''
+    Computes the contribution of n vertical wires at y_pos and z_pos
+    '''
+    x, y, z = coordinate
+    l_dis = 0.5*opt_design.opt_length
+    turns = opt_design.opt_turns
+    current = opt_design.opt_current   
+    av = ( (y-y_pos)**2 + (z-z_pos)**2 )**0.5
+    cos1v = (l_dis + x) / ((l_dis+x)**2 + av**2)**0.5
+    cos2v = (l_dis - x) / ((l_dis-x)**2 + av**2)**0.5
+    b0 = turns*Cage().u_0*current*(cos2v + cos1v)/(4*np.pi*av)
+    return b0
 
-class HelmCage:
+def sum_vertical_wires(coordinate):
+    '''
+    Computes the summation of vertical conductor at 4 different locations (with n wires)
+    '''
+    pn = vertical_wire(coordinate, +0.5*opt_design.opt_length, -0.5*opt_design.opt_distance)
+    pp = vertical_wire(coordinate, +0.5*opt_design.opt_length, +0.5*opt_design.opt_distance)
+    nn = vertical_wire(coordinate, -0.5*opt_design.opt_length, -0.5*opt_design.opt_distance)
+    np = vertical_wire(coordinate, -0.5*opt_design.opt_length, +0.5*opt_design.opt_distance)
+    # total
+    summation = pn + pp + nn + np
+    return summation
 
-    def __init__(self, n, i, a):
-        self.n = n
-        self.u0 = 4*np.pi*10**-7
-        self.i = i
-        self.l = 2*a
-        self.d = 2*0.5445*a
+def horizontal_wire(coordinate, x_pos, z_pos):
+    '''
+    Computes the contribution of n vertical wires at x_pos and z_pos
+    '''
+    x, y, z = coordinate
+    l_dis = 0.5*opt_design.opt_length
+    turns = opt_design.opt_turns
+    current = opt_design.opt_current    
+    ah = ((x - x_pos) ** 2 + (z - z_pos) ** 2) ** 0.5
+    cos1h = (l_dis + y) / ((l_dis + y) ** 2 + ah ** 2) ** 0.5
+    cos2h = (l_dis - y) / ((l_dis - y) ** 2 + ah ** 2) ** 0.5
+    b0 = turns*Cage().u_0*current*(cos2h + cos1h)/(4*np.pi*ah)
+    return b0
 
-    def ver_cond(self, x, y, z, y_pos, z_pos, i):
-        l_dis, d_dis = 0.5*self.l, 0.5*self.d
-        av = ( (y-y_pos)**2 + (z-z_pos)**2 )**0.5
-        cos1v = (l_dis + x) / ((l_dis+x)**2 + av**2)**0.5
-        cos2v = (l_dis - x) / ((l_dis-x)**2 + av**2)**0.5
+def sum_horizontal_wires(coordinate):
+    '''
+    Computes the summation of vertical conductor at 4 different locations (with n wires)
+    '''
+    xpzn = horizontal_wire(coordinate, +0.5*opt_design.opt_length, -0.5*opt_design.opt_distance)
+    xpzp = horizontal_wire(coordinate, +0.5*opt_design.opt_length, +0.5*opt_design.opt_distance)
+    xnzn = horizontal_wire(coordinate, -0.5*opt_design.opt_length, -0.5*opt_design.opt_distance)
+    xnzp = horizontal_wire(coordinate, -0.5*opt_design.opt_length, +0.5*opt_design.opt_distance)
+    # total
+    summation = xpzn + xpzp + xnzn + xnzp
+    return summation
 
-        b0 = self.n*self.u0*i*(cos2v + cos1v)/(4*np.pi*av)
-        z_hat = (y - l_dis)/av
-        y_hat = (z - z_pos)/av
-        return b0, z_hat, y_hat
-
-    def ver_coil(self, x, y, z):
-        # +x & -z
-        y_pos, z_pos = +0.5*self.l, -0.5*self.d
-        ypzn, z_pn, y_pn = HelmCage.ver_cond(self, x, y, z, y_pos, z_pos, +self.i)
-        # +x & +z
-        y_pos, z_pos = +0.5*self.l, +0.5*self.d
-        ypzp, z_pp, y_pp = HelmCage.ver_cond(self, x, y, z, y_pos, z_pos, +self.i)
-        # -x & -z
-        y_pos, z_pos = -0.5*self.l, -0.5*self.d
-        ynzn, z_nn, y_nn = HelmCage.ver_cond(self, x, y, z, y_pos, z_pos, +self.i)
-        # -x & +z
-        y_pos, z_pos = -0.5*self.l, +0.5*self.d
-        ynzp, z_np, y_np = HelmCage.ver_cond(self, x, y, z, y_pos, z_pos, +self.i)
-        # total
-        vertical = ypzn + ypzp + ynzn + ynzp
-        z = ypzn*z_pn + ypzp*z_pp + ynzn*z_nn + ynzp*z_np
-        y = ypzn * y_pn + ypzp * y_pp + ynzn * y_nn + ynzp * y_np
-        return vertical, z, y
-
-    def hor_cond(self, x, y, z, x_pos, z_pos, i):
-        l_dis, d_dis = 0.5*self.l, 0.5*self.d
-        ah = ((x - x_pos) ** 2 + (z - z_pos) ** 2) ** 0.5
-        cos1h = (l_dis + y) / ((l_dis + y) ** 2 + ah ** 2) ** 0.5
-        cos2h = (l_dis - y) / ((l_dis - y) ** 2 + ah ** 2) ** 0.5
-
-        b0 = self.n*self.u0*i*(cos2h + cos1h)/(4*np.pi*ah)
-        z_hat = (x - l_dis)/ah
-        x_hat = (z - z_pos)/ah
-        return b0, z_hat, x_hat
-
-    def hor_coil(self, x, y, z):
-        # +x & -z
-        x_pos, z_pos = +0.5*self.l, -0.5*self.d
-        xpzn, z_pn, x_pn = HelmCage.hor_cond(self, x, y, z, x_pos, z_pos, +self.i)
-        # +x & +z
-        x_pos, z_pos = +0.5 * self.l, +0.5 * self.d
-        xpzp, z_pp, x_pp = HelmCage.hor_cond(self, x, y, z, x_pos, z_pos, +self.i)
-        # -x & -z
-        x_pos, z_pos = -0.5 * self.l, -0.5 * self.d
-        xnzn, z_nn, x_nn = HelmCage.hor_cond(self, x, y, z, x_pos, z_pos, +self.i)
-        # -x & +z
-        x_pos, z_pos = -0.5 * self.l, +0.5 * self.d
-        xnzp, z_np, x_np = HelmCage.hor_cond(self, x, y, z, x_pos, z_pos, +self.i)
-        # total
-        horizontal = xpzn + xpzp + xnzn + xnzp
-        z = xpzn*z_pn + xpzp*z_pp + xnzn*z_nn + xnzp*z_np
-        x = + xpzn*x_pn + xpzp*x_pp + xnzn*x_nn + xnzp*x_np
-        return horizontal, z, x
-
-    def main(self):
-        b_field = np.zeros([2 * 200 + 1, 2 * 200 + 1])
-        x_field = np.zeros([2 * 200 + 1, 2 * 200 + 1])
-        y_field = np.zeros([2 * 200 + 1, 2 * 200 + 1])
-        z_field = np.zeros([2 * 200 + 1, 2 * 200 + 1])
-        for y_ind in range(-200, 200+1, 1):
-            y = y_ind/100
-            for z_ind in range(-200, 200+1, 1):
-                z = z_ind/100
-                bh, zh, xh = HelmCage.hor_coil(self, 0, y, z)
-                bv, zv, yv = HelmCage.ver_coil(self, 0, y, z)
-                x_field[y_ind + 200, z_ind + 200] = xh
-                b_field[y_ind+200, z_ind+200] = bh + bv  # B does not have any problem
-                z_field[y_ind+200, z_ind+200] = zh#+  #zv  # zh+zv  # horizontal does not show problem,
-                y_field[y_ind+200, z_ind+200] = yv  # vertical has a problem
-        plt.imshow(b_field, cmap="jet")
-        #plt.plot(145.55, 100, marker='o', color='black')
-        #plt.plot(145.55, 300, marker='o', color='black')
-        #plt.plot(254.45, 100, marker='o', color='black')
-        #plt.plot(254.45, 300, marker='o', color='black')
-        plt.title('Mag Field')
-        plt.colorbar()
-        plt.clim(0, 0.0004)
-        plt.show()
-        plt.savefig('cage.png')
+def main():
+    '''
+    Compute and save the vector field intensity around the center of the cage.
+    '''
+    step = 0.01  # 0.01 for centimeter or 0.001 for milimeter
+    b_range = [-2, 2]  # in meters
+    b_linspace = np.arange(b_range[0], b_range[1]+step, step)
+    b_intensity_map = np.zeros([np.size(b_linspace), np.size(b_linspace)])
+    coordinate = np.zeros(3)
+    for coordinate[1] in b_linspace:
+        for coordinate[2] in b_linspace:
+            b_from_hor_wires = sum_horizontal_wires(coordinate)  #print(b_from_hor_wires)
+            b_from_ver_wires = sum_vertical_wires(coordinate)  #print(b_from_ver_wires)
+            plane_index = (int((coordinate[1]+b_range[1])/step), \
+                           int((coordinate[2]+b_range[1])/step))
+            b_intensity_map[plane_index] = b_from_hor_wires + b_from_ver_wires
+    plt.imshow(b_intensity_map, cmap="jet")
+    plt.plot(145.55, 100, marker='o', color='black')
+    plt.plot(145.55, 300, marker='o', color='black')
+    plt.plot(254.45, 100, marker='o', color='black')
+    plt.plot(254.45, 300, marker='o', color='black')
+    plt.title('Magnetic Field around the center of the cage')
+    plt.colorbar()
+    plt.clim(0, 0.0004)
+    plt.show()
+    plt.savefig('cage.png')
 
 
 if __name__ == '__main__':
-    Cage = CageOpt()
-    a, i, n = Cage.opt_formulation()
-    helm_cage = HelmCage(n, i, a)
-    helm_cage.main()
+    opt_design = squared_cage_optimization()
+    main()
